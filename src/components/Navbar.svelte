ea12f58d32345e07a0964d9d3301ff7a6de7ca08
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2023-10-14 23:47:10 -0700

    Recreate front page from SvelteKit

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,117 @@
+<script lang="ts">
+  import { slide, fade } from "svelte/transition";
+
+  export let currentPath: string;
+
+  let scrollPosition = 0;
+  $: isScrolled = scrollPosition > 0;
+
+  const navbarContent = [
+    { name: "👨‍💻 Projects", href: "/programming" },
+    { name: "📷 Photography", href: "/photography" },
+    // { name: "📝 Articles", href: "/articles" },
+  ];
+
+  let collapsed = true;
+</script>
+
+<svelte:window bind:scrollY={scrollPosition} />
+
+<header
+  class="sticky top-0 z-10 bg-gray-50 transition-shadow ease-in"
+  class:shadow={isScrolled}
+>
+  <nav
+    class="mx-auto flex h-16 max-w-5xl flex-wrap items-center justify-between px-6"
+  >
+    <a
+      href="/"
+      data-sveltekit-preload-code="eager"
+      class="text-lg tracking-wide text-gray-600 decoration-yellow-400 decoration-2 underline-offset-8 duration-100 hover:text-gray-400"
+      class:underline={"/" === currentPath}
+    >
+      Mikkel Svartveit
+    </a>
+
+    <ul class="hidden space-x-8 md:flex">
+      {#each navbarContent as { name, href }}
+        <li>
+          <a
+            {href}
+            data-sveltekit-preload-code="eager"
+            class="text-lg tracking-wide text-gray-600 decoration-yellow-400 decoration-2 underline-offset-8 duration-100 hover:text-gray-400"
+            class:underline={href === currentPath}
+          >
+            {name}
+          </a>
+        </li>
+      {/each}
+    </ul>
+
+    <button
+      aria-label="Open menu"
+      class="md:hidden"
+      on:click={() => (collapsed = !collapsed)}
+    >
+      <svg
+        class="h-10 w-10 stroke-yellow-500"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g id="Menu / Hamburger_MD">
+          <path
+            id="Vector"
+            d="M5 17H19M5 12H19M5 7H19"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </g>
+      </svg>
+    </button>
+  </nav>
+</header>
+
+{#if !collapsed}
+  <div
+    transition:fade={{ duration: 100 }}
+    class="fixed left-0 top-0 z-20 h-full w-full bg-black opacity-50"
+  />
+  <ul
+    transition:slide={{ duration: 300, axis: "x" }}
+    class="fixed right-0 top-0 z-30 block h-full bg-white p-4"
+  >
+    <button
+      aria-label="Close menu"
+      on:click={() => (collapsed = true)}
+      class="ml-auto block h-10 w-10"
+    >
+      <svg
+        class="stroke-gray-400"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M16 8L8 16M8.00001 8L16 16"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </button>
+
+    {#each navbarContent as { name, href }}
+      <li class="my-6 ml-2 mr-16">
+        <a
+          {href}
+          on:click={() => (collapsed = true)}
+          class="whitespace-nowrap text-lg tracking-wide text-gray-600 decoration-yellow-400 decoration-2 underline-offset-8"
+          class:underline={href === currentPath}
+        >
+          {name}
+        </a>
+      </li>
+    {/each}
+  </ul>
+{/if}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      sans: "'Nunito', ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
+      serif:
+        "'Source Serif Pro', ui-serif, Georgia, Cambria, 'Times New Roman', Times, serif",
+      mono: "Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+    },
     extend: {},
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
   },
   plugins: [],
 };
