59a09ef61a1f240d6fab0cc403ebe1cfa85a69f7
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2023-10-18 17:58:57 -0700

    Update Tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
-      sans: "'Nunito', ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
-      serif:
-        "'Source Serif Pro', ui-serif, Georgia, Cambria, 'Times New Roman', Times, serif",
-      mono: "Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+      sans: ["'Nunito'", ...defaultTheme.fontFamily.sans],
+      serif: ["'Source Serif Pro'", ...defaultTheme.fontFamily.serif],
+      mono: [...defaultTheme.fontFamily.mono],
     },
     extend: {},
   },
