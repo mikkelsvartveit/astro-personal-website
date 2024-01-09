907e917b7893d07d9d0a156ad6697af0e75596c2
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2024-01-09 21:49:22 +0100

    Change to Cloudflare adapter to allow SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
+  output: "hybrid",
+  adapter: cloudflare({
+    imageService: "compile",
+  }),
 });
+
